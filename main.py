72277b400d2b71f3b56b44017fcb7b321f86eec5
rusern <rs.yeoh@estechs.com>
2021-04-13 21:16:59 +0800

    added checking for positive numbers

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ def main():
     print("hello world!")
 
 def sum(a, b):
-    return a + b
+    if (a>0 and b>0):
+        return a + b
 
 def multiply(a,b):
-    return a + b
+    if (a>0 and b>0):
+        return a + b
 
 if __name__ == "__main__":
     print("welcome to Ru Sern's app.......")
